6a97d5b87109d4e932e1fb88ed0ba65ae89be323
jvedock <janekvedock@comcast.net>
2021-09-28 09:53:31 -0400

    added velocity and direction setters to intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/core/Intake.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.lib.core;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class Intake {
+    private DcMotorEx motor1, motor2;
+
+    public Intake(DcMotorEx motor1, DcMotorEx motor2){
+        this.motor1 = motor1;
+        this.motor2 = motor2;
+    }
+
+
+
+
+
+    /************ Setters and Getters ************/
+
+    /**
+     *
+     * @param velocity the target velocity of the motor, in ticks per second
+     */
+    public void setVelocity(int velocity){
+        motor1.setVelocity(velocity);
+        motor2.setVelocity(velocity);
+    }
+
+    /**
+     *
+     * @param direction the intended direction of the motor
+     */
+    public void setDirection(DcMotorSimple.Direction direction){
+        motor1.setDirection(direction);
+        motor2.setDirection(direction);
+    }
+}
