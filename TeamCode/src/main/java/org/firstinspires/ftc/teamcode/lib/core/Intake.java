feda7e70e26a5e096ad0a3429f1df35b7342f4b2
jvedock <janekvedock@comcast.net>
2021-09-29 07:16:30 -0400

    fixed problem where intake motors were not inverted properly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/core/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lib/core/Intake.java
@@ -5,7 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class Intake {
     private DcMotorEx motor1, motor2;
-
+    private int velocity;
+    private DcMotorSimple.Direction direction;
     public Intake(DcMotorEx motor1, DcMotorEx motor2){
         this.motor1 = motor1;
         this.motor2 = motor2;
@@ -22,6 +23,7 @@ public class Intake {
      * @param velocity the target velocity of the motor, in ticks per second
      */
     public void setVelocity(int velocity){
+        this.velocity = velocity;
         motor1.setVelocity(velocity);
         motor2.setVelocity(velocity);
     }
@@ -31,21 +33,23 @@ public class Intake {
      * @param direction the intended direction of the motor
      */
     public void setDirection(DcMotorSimple.Direction direction){
+        this.direction = direction;
         motor1.setDirection(direction);
-        motor2.setDirection(direction);
+        if (direction== DcMotorSimple.Direction.FORWARD) {
+            motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+        else{
+            motor2.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
     }
 
-    public void getVelocity(int velocity){
-        motor1.getVelocity();
-        motor2.getVelocity();
+    public int getVelocity(int velocity){
+        return velocity;
     }
 
-    /**
-     *
-     * @param direction the intended direction of the motor
-     */
-    public void getDirection(DcMotorSimple.Direction direction){
-        motor1.getDirection();
-        motor2.getDirection();
+
+
+    public DcMotorSimple.Direction getDirection(){
+        return direction;
     }
 }
