10bf02491d51d93f106bae42454995ee4ca35ba9
jvedock <janekvedock@comcast.net>
2022-01-11 17:02:45 -0500

    more cleanup work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/v2/advent/field/FieldCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/v2/advent/field/FieldCore.java
@@ -5,20 +5,26 @@ public class FieldCore {
     private double xDim;
     private double yDim;
 
-
-
+    /**
+     * checks if a given point is not inside any areas in "forbidden areas"
+     *
+     * @param p the point to check
+     * @return boolean if it is a safe point (not in any areas)
+     */
     private boolean isSafe(Point p){
         for (SquareArea i : forbiddenAreas){
             if (i.inArea(p)){
                 return false;
             }
         }
+
         if (p.x>xDim ||p.x<0){
             return false;
         }
         if (p.y>yDim || p.y<0){
             return false;
         }
+
         return true;
     }
 
